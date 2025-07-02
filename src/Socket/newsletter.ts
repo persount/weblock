c37f64c613e4c404bd7ddfa07da99a5e838ebf73
persount <farrelza9@gmail.com>
2025-07-03 06:13:24 +0700

    Add files via upload

--- src/Socket/newsletter.ts
+++ src/Socket/newsletter.ts
@@ -175,7 +175,7 @@ export const makeNewsletterSocket = (config: SocketConfig) => {
                     description: description ?? null,
                     picture: picture ? (await generateProfilePicture(picture)).img.toString('base64') : null,
                     settings: {
-                        reaction_codes: { value: mode || 'ALL' }
+                        reaction_codes: mode || 'ALL'
                     }
                 }
             })
