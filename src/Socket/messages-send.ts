d37ba8cd64da8dfc67cdb16e8121a8a67a56317a
persount <farrelza9@gmail.com>
2025-07-17 16:20:26 +0700

    Add files via upload

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -783,7 +783,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
               attrs: { 
                  actual_actors: '2',
                  host_storage: '2',
-                 privacy_mode_ts: `${+new Date() / 1000 >>> 0}`
+                 privacy_mode_ts: +unixTimestampSeconds(new Date())
               }, 
            })
            didPushAdditional = true
